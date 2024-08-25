0fcd47e5698c7f78fdab1fc0f51ba60a0bc8f2b2
melodycious <melody.callejas@gmail.com>
2024-08-25 22:15:43 +0200

    json updated scripts

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from 'dotenv';
+
+
+// Configuration the .env file
+dotenv.config();
+
+// Create Express APP
+const app: Express = express();
+const port: string | number = process.env.PORT || 8000;
+
+// Define the first Route of APP
+app.get('/', (req: Request, res: Response) => {
+    // Send Hello World
+    res.send('Welcome to my supa dupa APP Express + TS + Swagger');
+});
+
+// Execute APP and Listen request to PORT
+app.listen(port, () => {
+    console.log(`EXPRESS SERVER: Running at http://localhost:${port}`)
+});
